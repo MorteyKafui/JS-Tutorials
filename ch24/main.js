6610fbdc1c05c05c271da5eebeaecaac0744bee7
Kafui Mortey <morteykafui@gmail.com>
2023-10-23 20:35:51 +0000

    select html elements in JS

--- ch24/main.js
+++ ch24/main.js
@@ -1,1 +1,13 @@
-document.title = "Hey There!";
+// const navbar = document.getElementsByTagName("nav");
+
+// const section = document.getElementsByClassName("container");
+
+// const btn = document.getElementById("btn");
+
+const navbar = document.querySelector("nav.navbar");
+
+const list = document.querySelectorAll(".list li");
+
+list.forEach(el => console.log(el));
+
+console.log(list);
