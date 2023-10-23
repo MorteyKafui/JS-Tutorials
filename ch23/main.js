8426ec7971f1ebf120237681c7003451af04f9a2
Kafui Mortey <morteykafui@gmail.com>
2023-10-23 06:26:44 +0000

    add chapter 23

---
+++ ch23/main.js
@@ -0,0 +1,19 @@
+// JavaScript Date Object
+// const date = new Date(2024, 0, 1, 6, 30, 20);
+const date = new Date(2020, 0, 1, 17, 10, 30);
+
+// console.log(date.toISOString());
+// console.log(date.toString());
+console.log(date.toLocaleString());
+
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDay();
+
+// const time = date.getTime();
+
+// console.log(time);
+// const d = Date.now();
+// console.log(d);
+
+// console.log(date);
